97ff892497542348b092f2a67b7b205810e38d43
carlangasbola <31832938+carlangasbola@users.noreply.github.com>
2017-09-11 08:55:59 -0500

    Add files via upload

---
+++ 07.- Sendredirect_Parametros/src/java/Servlet1.java
@@ -0,0 +1,27 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet1 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            String nombre = "Carlos Alberto Bolaños";
+            
+             response.sendRedirect("index.html?username=" + nombre );
+            
+        }
+    }
+
+ 
+
+    
+}
